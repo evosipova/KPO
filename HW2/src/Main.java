5dbd09e7ac2caeecf97dcca0c763bf9710479d36
evosipova <90273831+evosipova@users.noreply.github.com>
2022-11-19 02:47:06 +0300

    HW2

--- HW2/src/Main.java
+++ HW2/src/Main.java
@@ -62,7 +62,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        //Создаем шруппу студентов.
+        //Создаем группу студентов.
         for (int number = 0; number < random.nextInt(10); number++) {
             students.add(new Student("Name " + number, "Surname " + number, 0, true));
         }
